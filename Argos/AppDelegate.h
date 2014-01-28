57c8554860e129bcf8bc43da2f7fa099d0dee29d
Francis Tseng <ftzeng@gmail.com>
2014-01-27 20:15:58 -0500

    Initial Commit

---
+++ Argos/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Argos
+//
+//  Created by Francis Tseng on 1/27/14.
+//  Copyright (c) 2014 Argos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ArgosTests/ArgosTests.m
@@ -0,0 +1,34 @@
+//
+//  ArgosTests.m
+//  ArgosTests
+//
+//  Created by Francis Tseng on 1/27/14.
+//  Copyright (c) 2014 Argos. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ArgosTests : XCTestCase
+
+@end
+
+@implementation ArgosTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
